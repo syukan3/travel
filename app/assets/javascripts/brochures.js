d48b0f6b667284ae46d94649cea79b169866878f
syukan3 <en421430@s.okayama-u.ac.jp>
2019-01-29 20:12:40 +0900

    1/29-1

--- app/assets/javascripts/brochures.js
+++ app/assets/javascripts/brochures.js
@@ -57,39 +57,43 @@ function initMap() {
       });
     });
 
-    autocomplete.addListener('place_changed', function() {
-              infowindow.close();
-              marker.setVisible(false);
-              var place = autocomplete.getPlace();
-              if (!place.geometry) {
-                // User entered the name of a Place that was not suggested and
-                // pressed the Enter key, or the Place Details request failed.
-                window.alert("No details available for input: '" + place.name + "'");
-                return;
-              }
+    autocompleteDep.addListener('place_changed', function() {
+      var place = autocompleteDep.getPlace();
+      console.log(place.geometry.location);
+      var marker = new google.maps.Marker({
+        position: place.geometry.location,
+        map: this,
+        animation: google.maps.Animation.DROP
+      });
+      if (!place.geometry) {
+        // User entered the name of a Place that was not suggested and
+        // pressed the Enter key, or the Place Details request failed.
+        window.alert("No details available for input: '" + place.name + "'");
+        return;
+      }
 
-              // If the place has a geometry, then present it on a map.
-              if (place.geometry.viewport) {
-                map.fitBounds(place.geometry.viewport);
-              } else {
-                map.setCenter(place.geometry.location);
-                map.setZoom(17);  // Why 17? Because it looks good.
-              }
-              marker.setPosition(place.geometry.location);
-              marker.setVisible(true);
-              var address = '';
-          if (place.address_components) {
-            address = [
-              (place.address_components[0] && place.address_components[0].short_name || ''),
-              (place.address_components[1] && place.address_components[1].short_name || ''),
-              (place.address_components[2] && place.address_components[2].short_name || '')
-            ].join(' ');
-          }
+      // If the place has a geometry, then present it on a map.
+      if (place.geometry.viewport) {
+        map.fitBounds(place.geometry.viewport);
+      } else {
+        map.setCenter(place.geometry.location);
+        map.setZoom(17);  // Why 17? Because it looks good.
+      }
+      marker.setPosition(place.geometry.location);
+      marker.setVisible(true);
+      var address = '';
+  if (place.address_components) {
+    address = [
+      (place.address_components[0] && place.address_components[0].short_name || ''),
+      (place.address_components[1] && place.address_components[1].short_name || ''),
+      (place.address_components[2] && place.address_components[2].short_name || '')
+    ].join(' ');
+  }
 
-          infowindowContent.children['place-icon'].src = place.icon;
-          infowindowContent.children['place-name'].textContent = place.name;
-          infowindowContent.children['place-address'].textContent = address;
-          infowindow.open(map, marker);
+  infowindowContent.children['place-icon'].src = place.icon;
+  infowindowContent.children['place-name'].textContent = place.name;
+  infowindowContent.children['place-address'].textContent = address;
+  infowindow.open(map, marker);
         });
 
   });
